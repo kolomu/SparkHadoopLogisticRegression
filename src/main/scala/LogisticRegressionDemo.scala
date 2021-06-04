57998c6434f3b3537a70a143fb566775bc997f46
kolomu <kevin-olomu@web.de>
2021-06-04 14:13:19 +0200

    remove comments LogisticRegressionDemo

--- src/main/scala/LogisticRegressionDemo.scala
+++ src/main/scala/LogisticRegressionDemo.scala
@@ -14,12 +14,11 @@ object LogisticRegressionDemo {
       .appName("LogisticRegressionExample")
       .getOrCreate()
 
-    spark.sparkContext.setLogLevel("ERROR")
+    spark.sparkContext.setLogLevel("OFF")
 
     // load dataset
     // val csvPath = getClass.getResource("/fraud.csv").getPath
     val csvPath  = "hdfs://namenode:9000/data/csv/fraudexample/fraud.csv"
-    // /user/root/input
     val df =  spark.read
       .option("header", true) // file contains header columns, we want to refer to them in the select statement
       .option("inferSchema", true) // when setting to true it automatically infers column types based on the data
@@ -27,7 +26,7 @@ object LogisticRegressionDemo {
       .select("type", "amount", "oldbalanceOrg", "newbalanceOrig", "isFraud")
 
     df.printSchema()
-    // df.show(2)
+    df.show(2)
 
     // split the dataset into test and training data
     val result = df.randomSplit(Array(0.7, 0.3), 7)
@@ -35,27 +34,21 @@ object LogisticRegressionDemo {
     val testDF: DataFrame = result.tail.head
 
 
-    // println(s"Train set length ${trainDF.count()} records")
-    // println(s"Test set length ${testDF.count()} records")
+    println(s"Train set length ${trainDF.count()} records")
+    println(s"Test set length ${testDF.count()} records")
 
-    // trainDF.show(2)
+    trainDF.show(2)
 
     // check the data type
     // we need to check the data type because any type of string is treated as categorical variable
-    // df.dtypes: Returns all column names and their data types as an array.
-    // https://spark.apache.org/docs/latest/api/scala/org/apache/spark/sql/Dataset.html
     val catCols = trainDF.dtypes.filter( value =>  value._2 == "StringType")
     val numCols = trainDF.dtypes.filter( value => value._1 != "isFraud" && value._2 == "DoubleType")
 
-    // StringIndexer: Converts a single feature to an index feature. http://spark.apache.org/docs/latest/ml-features#stringindexer
-    // OneHotEncoder: http://spark.apache.org/docs/latest/ml-features#onehotencoder
-    // For more info: http://spark.apache.org/docs/latest/ml-features
-
     // how many different types are there?
-    // trainDF.select(countDistinct("type")).show()
+    trainDF.select(countDistinct("type")).show()
 
     // group by types
-    // trainDF.groupBy("type").count().show()
+    trainDF.groupBy("type").count().show()
 
     // setting up string indexer
     val string_indexer = new StringIndexer()
@@ -69,7 +62,6 @@ object LogisticRegressionDemo {
       .setOutputCols(Array(catCols.head._1 + "_OneHotEncoder"))
 
     // vector assembling: combines values of input columns into a single vector
-    // https://spark.apache.org/docs/latest/ml-features#vectorassembler
     val assemblerInput = numCols.map( value => value._1)
     assemblerInput :+ catCols.map( value => value._1 + "_OneHotEncoder")
     val vectorAssembler = new VectorAssembler()
@@ -81,29 +73,21 @@ object LogisticRegressionDemo {
     val pipelineModel = pipeline.fit(trainDF)
     val pp_df = pipelineModel.transform(testDF)
 
-    // pp_df.select("type", "amount", "oldbalanceOrg", "newbalanceOrig", "VectorAssembler_features").show()
+    pp_df.select("type", "amount", "oldbalanceOrg", "newbalanceOrig", "VectorAssembler_features").show()
 
     val mldata = pp_df.select(
       col("VectorAssembler_features").as("features"),
       col("isFraud").as("label"))
 
-    // mldata.show(5, false)
+    mldata.show(5, false)
 
-    println("Starting to train the model")
+    println("training the model")
     val logisticRegressionModel = new LogisticRegression()
-      .setTol(0.1) // Fix LBFGS Error
       .fit(mldata)
     println(s"Coefficients: ${logisticRegressionModel.coefficients} Intercept: ${logisticRegressionModel.intercept}")
 
     val trainingSummary = logisticRegressionModel.binarySummary
 
-    // Calculate Area Under the ROC Curve
-    // metric for classification problem
-    // 1 = good measure of seperability
-    // 0 = worst separability
-    // https://towardsdatascience.com/understanding-auc-roc-curve-68b2303cc9c5
-    // https://www.graphpad.com/guides/prism/8/curve-fitting/reg_logistic_roc_curves.htm
-    // choose cutoff with high sensitivy
     val roc = trainingSummary.roc
     roc.show()
 
